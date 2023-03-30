1eed03f696f2de2301012046c60a85dd01460449
yusuffardian <yusuffardian09@gmail.com>
2023-03-30 10:00:49 +0700

    Dimas Pratama Putra

---
+++ 04220036.py
@@ -0,0 +1,3 @@
+# Tulis Nama dan NIM kamu sebagai penanda kamu sudah siap menggunakan GIT
+print("Berikut nama-nama yang sudah clone, modify, commit dan push file ini:")
+print("No.035 Dimas Pratama Putra (04220036)")
