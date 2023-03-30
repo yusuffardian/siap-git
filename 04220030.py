431fe270709dcf983325f1af9090cbb8332c007f
yusuffardian <dimaspratama450@gmail.com>
2023-03-30 10:12:09 +0700

    Muhammad Yusuf Fardiansyah

--- 04220030.py
+++
@@ -1,4 +0,0 @@
-#Tulis Nama dan Nim Untuk Penanda Anda telah siap menggunakan git
-print("Berikut nama-nama yang sudah clone, modify, commit dan push file ini :")
-print("No. Nama-Lengkap (NIM-Anda)")
-print ("033. Muhammad Yusuf Fardiansyah (04220030)")
